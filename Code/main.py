0e9e6264fc4ea4f752213799262e579c16031772
Maan <maandammag456@gmail.com>
2025-09-15 00:42:50 +0300

    This is the first Code uploaded

---
+++ Code/main.py
@@ -0,0 +1,31 @@
+import sys 
+
+"""
+Each one write his code in a function and all functions calls should be in the gui function
+"""
+class mainapp:
+    def __init__(self):
+        # Initialize the main application
+        print("Main Application Initialized")
+    def project_gui():
+        # this function for project GUI
+        print("Project GUI")
+        
+
+def fault_label():
+    # this function for fault label model
+    print("Fault Label")
+
+def fault_type():
+    # this function for fault type model
+    print("Fault Type")
+
+def fault_rul():
+    # this function for fault RUL model
+    print("Fault RUL") 
+
+def project_gui():
+    # this function for project GUI
+    print("Project GUI")
+
+print("Welcome to the Fault Diagnosis System")
